21de2884ec6eadbed48eaa3047bf40e89828e3a1
Alexandre de Souza <89175716+xdtr27@users.noreply.github.com>
2026-05-26 03:10:20 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,106 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
+
+// Middleware
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const customer = users.find(user => user.username === username)
+
+  if(!customer) {
+    return response.status(404).json({
+      error: "User not found"
+    })
+  }
+
+  request.customer = customer;
+
+  next()
 }
 
+
+// Cadastrar usuário
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  const customerAlreadyExists = users.some(user => user.username === username)
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists"
+    })
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  })
+
+  return response.status(201).json({
+    message: "User created sucessfully"
+  })
 });
 
+// Buscar lista de tasks de um usuário
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {customer} = request;
+
+  return response.status(200).json({
+    status: 200,
+    todos: customer.todos
+  })
 });
 
+// Criar task
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {customer} = request;
+  const {deadLine, title} = request.body;
+
+  customer.todos({
+    id: uuidv4(),
+    title,
+    done: false,
+    deadLine: new Date(deadLine),
+    created_at: new Date()
+  })
+
+  return response.status(201).json({
+    message: "Task created sucessfully"
+  })
 });
 
+// Buscar task por id
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const idTask = request.params.id;
+  const {customer} = request;
+  const {title, deadLine} = request.body;
+
+  const task = customer.todos.find(task => task.id === idTask)
+
+  if(!task) {
+    return response.status(404).json({
+      error: "No task with that ID was found associated with that user."
+    })
+  }
+
+  if(title) task.title = title
+  if(deadLine) task.deadLine = deadLine
+
+  return response.status(200).json({
+    message: "Task changed successfully"
+  })
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
